a7cae3be21a536dba71721b1590d5f0576a5621f
GregWagner <theGreg101@gmail.com>
2020-11-17 07:49:48 -0600

    Finished section 1

--- Section_01_Getting_Started_with_Typescript/fetchjson/index.js
+++ Section_01_Getting_Started_with_Typescript/fetchjson/index.js
@@ -3,5 +3,7 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = "https://jsonplaceholder.typicode.com/todos/1";
 axios_1["default"].get(url).then(function (response) {
-    console.log(response.data);
+    var todo = response.data;
+    var ID = todo.ID, title = todo.title, finished = todo.finished;
+    console.log("\n        The Todo with ID: " + ID + "\n        Has a title of: " + title + "\n        Is it finished? " + finished);
 });
